b48d1cb9daab64c9532472aa5cfde03cd33cc276
rju <rju@purdue.edu>
2017-03-08 09:11:33 -0500

    Added testJapanese.py to Conjugation.py

--- conjugation.py
+++ conjugation.py
@@ -1,5 +1,8 @@
 # Conjugation.py
 
+import unicodedata
+import sys
+
 # Functions Needed
 # Dictionary -> Masu
 # Masu -> Dictionary
@@ -8,4 +11,36 @@
 # Masu -> Mashita (Past Tense)
 # Mashita -> Masu
 
+
+# Copied over from testJapanese.py in python-scripts REPO
+# dictionaryForm = raw_input("Dictionary Form: ")
+# # print "Dictionary Form: " + dictionaryForm
 # 
+# masuForm = dictionaryForm.decode("utf-8")
+# stem_length = len(masuForm) - 2
+# 
+# masu = 'ます'
+# 
+# # Ru-Verbs
+# # print masuForm[:stem_length+1] + masu.decode("utf-8")
+# 
+# # U-Verbs
+# # utoe = unichr(ord(masuForm[stem_length+1])-1)
+# # print "Masu Form: " + masuForm[:stem_length+1] + utoe + masu.decode("utf-8")
+# 
+# # Irregular Verbs
+# if (masuForm == ('いく').decode("utf-8")):
+# 	masuForm = ('行き').decode("utf-8")
+# 	print masuForm + masu.decode("utf-8")
+# elif (masuForm == ('くる').decode("utf-8")):
+# 	masuForm = ('来').decode("utf-8")
+# 	print masuForm + masu.decode("utf-8")
+# else:
+# 	print "Verb is neither"
+# 	
+# # print masuForm + masu.decode("utf-8")
+# 
+# 
+# # Other forms still needed: Volitional, Te-Form, Ta-Form?, Negative, Potential
+
+# Either functions with dic -> masu, masu -> dic, etc. or object initialized with dictionary form
