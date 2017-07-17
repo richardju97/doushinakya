e21a7cf46ce14a35225c23e5d0b01db981fcdde6
rju <rju@purdue.edu>
2017-07-17 20:50:42 +0800

    Changed URL to search a japanese word

--- testJishoAPI.py
+++ testJishoAPI.py
@@ -1,19 +1,22 @@
 # Python file for testing jisho.org's API
 
 # link to their API, note that their API is also a work in progress
-# http://jisho.org/api/v1/search/words?keyword=amazing
+# http://jisho.org/api/v1/search/words?keyword=食べる
 
-url = "http://jisho.org/api/v1/search/words?keyword=amazing"
+url = "http://jisho.org/api/v1/search/words?keyword=食べる"
 
 import json
 import requests
 
-r = requests.get(url)
-
-status = r.json()['meta']['status']
+r = requests.get(url).json()
+status = r['meta']['status']
 
 if (status == 200):
     print("Connection to jisho.org successful")
+
+    data = r['data']
+    print (data)
+
 else:
     print ("Error connecting to jisho.org: " + str(status))
 
