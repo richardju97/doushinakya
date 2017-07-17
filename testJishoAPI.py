3ab537b1a333ea18dbac6e0c39359940125b0f35
rju <rju@purdue.edu>
2017-07-17 20:29:15 +0800

    Successfully called JSON data

--- testJishoAPI.py
+++ testJishoAPI.py
@@ -3,4 +3,11 @@
 # link to their API, note that their API is also a work in progress
 # http://jisho.org/api/v1/search/words?keyword=amazing
 
+url = "http://jisho.org/api/v1/search/words?keyword=amazing"
+
+import json
+import requests
+
+r = requests.get(url)
+print (r.json())
 
