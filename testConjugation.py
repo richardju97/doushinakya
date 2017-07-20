ef44c15cb8f5bc5a7157b337892255cb55473386
rju <rju@purdue.edu>
2017-07-20 09:47:36 +0800

    Comments w/ what tests are needed

--- testConjugation.py
+++ testConjugation.py
@@ -1,3 +1,54 @@
 # Test Case File for conjugation.py
 
 import conjugation
+
+#Test 1:
+#Ichidan Verbs
+#
+#Test 2:
+#Godan Verbs w/　う
+#
+#Test 3:
+#Godan Verbs w/　く
+#
+#Test 4:
+#Godan Verbs w/ ぐ
+#
+#Test 5:
+#Godan Verbs w/　す
+#
+#Test 6:
+#Godan Verbs w/ ず
+#
+#Test 7:
+#Godan Verbs w/　つ
+#
+#Test 8:
+#Godan Verbs / づ
+#
+#Test 9:
+#Godan Verbs w/ ぬ
+#
+#Test 10:
+#Godan Verbs w/ ふ
+#
+#Test 11:
+#Godan Verbs w/ ぶ
+#
+#Test 12:
+#Godan Verbs w/ ぷ
+#
+#Test 13:
+#Godan Verbs w/ む
+#
+#Test 14:
+#Godan Verbs w/ る
+#
+#Test 15:
+#Godan Verbs w/ ゆ
+#
+#Test 16:
+#Irregular Verb -　行く
+#
+#Test 17:
+#Irregular Verb - 来る
