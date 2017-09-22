ebe868819926ab12da3ebe92af4fc9253ba53c17
rju <rju@purdue.edu>
2017-09-22 14:35:16 -0400

    Commit by Testall on 2017-09-22 14:35:16.677376

--- conjugation.py
+++ conjugation.py
@@ -131,7 +131,7 @@ class doushi:
             
         # Volitional Form
         u = 'う'
-        utoo = chr(ord(self.forms['Dictionary Form'][self.length+1]) + 3)
+        utoo = chr(ord(self.forms['Dictionary Form'][self.length+1]) + 4)
         self.forms['Volitional'] = (stem + utoo + u)
             
         
