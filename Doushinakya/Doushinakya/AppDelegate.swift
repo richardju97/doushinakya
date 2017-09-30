127a6336f1da730d46bbec42be5e9936676b1ebe
rju <rju@purdue.edu>
2017-09-30 03:36:43 -0400

    INIT Mac OS X Project

---
+++ Doushinakya/Doushinakya/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Doushinakya
+//
+//  Created by Richard Ju on 9/30/17.
+//  Copyright © 2017 Richard Ju. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Doushinakya/Doushinakya/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Doushinakya
+//
+//  Created by Richard Ju on 9/30/17.
+//  Copyright © 2017 Richard Ju. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
