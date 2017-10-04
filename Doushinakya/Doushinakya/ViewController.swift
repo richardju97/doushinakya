ab4d0573685fdd9604966aa36dd62a1eddfc04c7
rju <rju@purdue.edu>
2017-10-04 03:59:02 -0400

    Changes label to inputted text

--- Doushinakya/Doushinakya/ViewController.swift
+++ Doushinakya/Doushinakya/ViewController.swift
@@ -23,11 +23,22 @@ class ViewController: NSViewController {
     }
 
     @IBOutlet weak var VerbDic: NSTextField!
-
     @IBOutlet weak var DispVerb: NSTextField!
     
     @IBAction func Submit(_ sender: NSButton) {
-        DispVerb.stringValue = "Testing"
+        let url = URL(string: "http://jisho.org/api/v1/search/words?keyword=食べる")
+
+//        let task = URLSession.shared.dataTask(with: url! as URL) { data, response, error in
+//
+//            guard let data = data, error == nil else { return }
+//            print(NSString(data: data, encoding: String.Encoding.utf8.rawValue))
+//        }
+//        
+//        task.resume()
+        
+        let doushi = VerbDic.stringValue
+        
+        DispVerb.stringValue = doushi
     }
 }
 
