ca8d334733cca01e28ae429c8f7f5b6425fb878c
rju <rju@purdue.edu>
2017-11-25 12:38:44 -0500

    Testing views.py

--- conjugate/views.py
+++ conjugate/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def hello(request):
+    text = """<h1>Welcome to Doushinakya!</h1>"""
+    return HttpResponse(text)
